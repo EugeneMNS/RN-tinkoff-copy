a980c9754ea837a577438466fda0ab603af2357d
EugeneMNS <evgen.str4@gmail.com>
2022-11-03 16:12:36 +0300

    feat: more & services

--- app/componets/screens/services/Services.tsx
+++ app/componets/screens/services/Services.tsx
@@ -1,12 +1,23 @@
-import React, {FC} from "react";
-import {View, Text} from "react-native";
+import React, { FC } from 'react'
+import { View, Text } from 'react-native'
+import tw from 'tailwind-rn'
+
+import Layout from "../../layouts/Layout";
+import { services } from './data'
+import ServiceItem from './item/ServiceItem'
+
 
 const Services: FC = () => {
-    return(
-        <View>
-            <Text>Services</Text>
-        </View>
-    )
+	return (
+		<Layout>
+			<Text style={tw('text-center text-lg mb-6')}>Moscow</Text>
+			<View style={tw('flex-row flex-wrap justify-center')}>
+				{services.map(service => (
+					<ServiceItem key={service.title} service={service} />
+				))}
+			</View>
+		</Layout>
+	)
 }
 
 export default Services
