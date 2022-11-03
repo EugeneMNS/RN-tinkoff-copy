b1bde770d41eae5fb56df0b91461929135f4c8c7
EugeneMNS <evgen.str4@gmail.com>
2022-11-03 08:33:01 +0300

    feat: bank account

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import {AuthProvider} from "./app/providers/AuthProvider";
 import Navigation from "./app/navigation/Navigation";
 import {LogBox} from "react-native";
 import {DataProvider} from "./app/providers/DataProvider";
-import StoryContainer from "./app/componets/screens/profile/stories/StoryContainer";
+import StoryContainer from "./app/componets/screens/home/stories/StoryContainer";
 
 export default function App() {
   return <AuthProvider>
--- app/componets/screens/home/Home.tsx
+++ app/componets/screens/home/Home.tsx
@@ -2,13 +2,17 @@ import React, {FC} from "react";
 import {Text} from "react-native";
 import Header from "./Header";
 import Layout from "../../layouts/Layout";
-import Stories from "../profile/stories/Stories";
+import Stories from "./stories/Stories";
+import Accounts from "./accounts/Accounts";
+import ApplyNewProduct from "./apply-new-products/ApplyNewProduct";
 
 const Home : FC = () => {
     return(
         <Layout>
             <Header />
             <Stories />
+            <Accounts />
+            <ApplyNewProduct />
         </Layout>
     )
 }
